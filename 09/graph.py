4cefecdd60896ef84a3baa5bbd6af55750d80f03
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-07-05 21:22:47 +0900

    update

--- 09/graph.py
+++ 09/graph.py
@@ -1,18 +1,33 @@
+from sqlite3 import DatabaseError
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 fig = plt.figure(dpi=120)
 
-data = pd.read_csv('b_result.csv', encoding = 'UTF-8')
+# open file #
+bubble = pd.read_csv('b_result.csv', encoding = 'UTF-8')
+select = pd.read_csv('s_result.csv', encoding = 'UTF-8')
 
-int result[]
-print(data)
+print(bubble)
+print(select)
 
-for i in data:
-    
-data_x = data[data.columns[1]]
-data_y = data[data.columns[2]]
+# col select #
+column1_bubble = bubble[bubble.keys()[0]]
+column2_bubble = bubble[bubble.keys()[1]]
 
-plt.plot(data_x, data_y, linestyle='solid', marker='o')
+column1_select = select[select.keys()[0]]
+column2_select = select[select.keys()[1]]
+
+
+# LAVEL #
+plt.xlabel(bubble.keys()[0])
+plt.ylabel(bubble.keys()[1])
+
+# Plot #
+plt.plot(column1_bubble, column2_bubble, linestyle='solid', marker='o')
+plt.plot(column1_select, column2_select, linestyle='solid', marker='o')
+
+# Han rei #
+plt.legend(["Bubble sort", "Select sort"])
 
 fig.savefig("img.png")
