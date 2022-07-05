6db748db32f587cacca39b7488180b9e6b97f8a1
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-07-05 14:57:50 +0900

    support CSV out

--- 09/4-5bubble.c
+++ 09/4-5bubble.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 2000
+#define SIZE 1000
 
 void output( int data[], int size ){
     int i;
@@ -16,10 +16,23 @@ int main(void){
     unsigned long int end; /* 実行後の時間(ミリ秒)*/
     unsigned long int elapsed; /* 実行時間(ミリ秒)*/
     
+    /*--- 宣言 ---*/
     int* data;
     int size = SIZE;
     int i, j, tmp, seed;
-    
+
+    /*--- CSV ---*/
+    FILE *fp;
+    fp = fopen("./b_result.csv", "w");
+    if(fp == NULL) {
+        printf( "File open ERROR Deesu" );
+        return -1;
+    }
+
+    fprintf(fp, "data,number,time(1/%lu[s])\n",CLOCKS_PER_SEC);
+
+    printf("\n %lu \n", CLOCKS_PER_SEC);
+
     printf("Seed?=");
     scanf("%d", &seed);
     srand(seed);
@@ -45,16 +58,18 @@ int main(void){
                     }
                 }
             }
-            
-            
+
             end = clock(); /* 処理後の時間を取得 */
             elapsed = end - start; /* 実行時間算出 */
             printf("ELAPSED TIME %d: %lu\n", (loop+1), elapsed);
-
+            
             free(data);
         }
+        fprintf(fp, "%d,%d,%lu\n", size, (loop+1), elapsed);
+        free(data);
         size += SIZE;
         printf("\n--------\n");
     }
+    fclose(fp);
     return 0; 
 }
